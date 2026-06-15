d078589fdabdfcb2c740465a1f18ed815fdfae38
Murali14898 <samalmurali575@gmail.com>
2026-06-15 11:50:09 +0530

    basic java microservice communication

---
+++ communication/SimpleJavaCommuntication/ProductMicroservice/src/main/java/com/microservice/product/ProductMicroservice/controller/ProductController.java
@@ -0,0 +1,24 @@
+package com.microservice.product.ProductMicroservice.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.microservice.product.ProductMicroservice.models.Product;
+import com.microservice.product.ProductMicroservice.services.ProductService;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+	
+	@Autowired
+	ProductService productService;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Product> getProduct(@PathVariable String id){
+		return productService.getProduct(id);
+	}
+}
